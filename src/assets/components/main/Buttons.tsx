530436105c097a77abe527a511ab449f1e059ee0
Ilya Alikin <iljuha.alikin@gmail.com>
2024-07-22 16:49:07 +0500

    add responsive main screen

--- src/assets/components/main/Buttons.tsx
+++ src/assets/components/main/Buttons.tsx
@@ -16,14 +16,16 @@ const info = [
 const Buttons = () => {
   return (
     <>
-      <div className="flex justify-between">
+      <div className="flex justify-between flex-col gap-2 sm:flex-row lg:flex-row xl:flex-row 2xl:flex-row">
         {info.map((item, index) => {
           return (
-            <div className="flex gap-2 justify-center align-center">
-              <div className="text-4xl flex align-center font-medium justify-center">
+            <div className="flex gap-1 lg:gap-2 justify-center align-center">
+              <div className="w-[40px] text-xl lg:text-3xl xl:text-4xl flex align-center font-medium justify-center">
                 {item.number}
               </div>
-              <div className="max-w-[100px]">{item.title}</div>
+              <div className="max-w-[100px] text-sm lg:text-md xl:text-lg">
+                {item.title}
+              </div>
             </div>
           );
         })}
--- src/assets/components/main/Content.tsx
+++ src/assets/components/main/Content.tsx
@@ -2,15 +2,15 @@ import Buttons from "../main/Buttons";
 const Content = () => {
   return (
     <>
-      <div className="container mx-auto flex flex-col py-[150px] xl:py-0 lg:py-0 md:py-0 xl:items-center md:items-center xl:justify-between xl:flex-row md:justify-between md:flex-row lg:align-center lg:justify-center lg:flex-row h-screen relative z-10 gap-8 px-4">
+      <div className="container mx-auto flex flex-col-reverse py-[150px] xl:py-0 lg:py-0 md:py-0 xl:items-center md:items-center xl:justify-between xl:flex-row md:justify-between md:flex-row lg:align-center lg:justify-center lg:flex-row h-screen relative z-10 gap-8 px-4">
         <div className="flex flex-col gap-8">
           <div className="flex flex-col gap-6 lg:gap-8 xl:gap-12">
             <div className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-medium">
               Hello, I’m Ilya -<span className="block">Frontend developer</span>
             </div>
-            <button className="text-lg lg:text-xl xl:text-2xl rounded-full py-2 lg:py-3 xl:px-10 xl:py-4 uppercase bg-primary-color text-white inline-block max-w-[345px]">
+            {/* <button className="text-lg lg:text-xl xl:text-2xl rounded-full py-2 lg:py-3 xl:px-10 xl:py-4 uppercase bg-primary-color text-white inline-block max-w-[345px]">
               Contact me
-            </button>
+            </button> */}
           </div>
           <Buttons />
         </div>
